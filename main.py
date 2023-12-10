3cfe546cf03d950aed6eb153077f57d92a4b3449
Nima Mehrafar <nima.mehrafar@beequip.nl>
2023-12-11 00:46:32 +0100

    Multihead NN

--- main.py
+++ main.py
@@ -20,62 +20,86 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Dense
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
+import re
+
 
 
 # load in Data
-Porto_data = pd.read_csv(r'C:\Users\NimaMehrafar\UC\UC_Project\porto_data_2')
-SanFrancisco_data = pd.read_csv(r'C:\Users\NimaMehrafar\UC\UC_Project\san_francisco_data_2')
-Tokyo_data = pd.read_csv(r'C:\Users\NimaMehrafar\UC\UC_Project\tokyo_data_2')
+Porto_data = pd.read_csv(r'C:\Users\NimaMehrafar\UC\UC_Project\crossing_porto')
+SanFrancisco_data = pd.read_csv(r'C:\Users\NimaMehrafar\UC\UC_Project\crossing_san_francisco')
+Tokyo_data = pd.read_csv(r'C:\Users\NimaMehrafar\UC\UC_Project\crossing_tokyo')
 
-clf = KMeansConstrained(
-     n_clusters=85,
-    size_min=40,
-    size_max=60, 
-    random_state=0)
+
+def extract_lon_lat(point_string):
+    nums = re.findall(r"[-+]?\d*\.\d+|\d+", point_string)
+    return float(nums[0]), float(nums[1])
 
 #Preprocessing
-Porto_data = Porto_data[['element_type','highway','n_type']]
-label_encoder = LabelEncoder()
-Porto_data['highway'] = label_encoder.fit_transform(Porto_data['highway'])
-Porto_data['element_type'] = label_encoder.fit_transform(Porto_data['element_type'])
+def preprocess(dataframe):
+      label_encoder = LabelEncoder()
+      dataframe['highway_tag'] = label_encoder.fit_transform(dataframe['highway_tag'])
+      dataframe['element_type'] = label_encoder.fit_transform(dataframe['element_type'])
+      dataframe['geometry_float'] = dataframe['geometry'].apply(lambda x: pd.Series(extract_lon_lat(x))).prod(axis=1)
+      dataframe = dataframe[['element_type','highway_tag','geometry_float']]
+
+      return dataframe
+
+Porto_data = preprocess(Porto_data)
+
+clf_Porto = KMeansConstrained(
+     n_clusters=int(round(len(Porto_data)/100)),
+    size_min=90,
+    size_max=100, 
+    random_state=0)
 
 #Clustering###
-group_size = 30
-clf.fit_predict(Porto_data.values)
-clusters = clf.labels_
+clf_Porto.fit_predict(Porto_data.values)
+clusters = clf_Porto.labels_
 Porto_data['cluster'] = clusters
 
-X1 = Porto_data[['element_type', 'highway']].values
-X2 = Porto_data[['element_type', 'n_type']].values
-X3 = Porto_data[['highway', 'n_type']].values
-X_combined = np.concatenate([X1, X2, X3], axis=1)
 
-Y1 = Porto_data['n_type'].values - 1 
-Y2 = Porto_data['highway'].values
-Y3 = Porto_data['element_type'].values
+def NeuralNet(dataframe):
+      dataframe = dataframe[['element_type','highway_tag','geometry_float']]
+      X1 = dataframe[['element_type', 'geometry_float']].values
+      X2 = dataframe[['highway_tag', 'geometry_float']].values
+      X3 = dataframe[['element_type', 'highway_tag']].values
+      X_combined = np.concatenate([X1, X2, X3], axis=1)
 
-X_train, X_test, Y_train, Y_test = train_test_split(X_combined, np.column_stack((Y1, Y2, Y3)), test_size=0.2, random_state=42)
+      Y1 = dataframe['highway_tag'].values
+      Y2 = dataframe['element_type'].values 
+      Y3 = dataframe['geometry_float'].values
 
-input_layer = Input(shape=(6,))
-shared_layers = Dense(64, activation='relu')(input_layer)
-shared_layers = Dense(32, activation='relu')(shared_layers)
+      X_train, X_test, Y_train, Y_test = train_test_split(X_combined, np.column_stack((Y1, Y2, Y3)), test_size=0.2, random_state=42)
+
+      input_layer = Input(shape=(6,))
+      shared_layers = Dense(64, activation='relu')(input_layer)
+      shared_layers = Dense(64, activation='relu',name='finaldense')(input_layer)
+
+      output_task1 = Dense(10, activation='softmax', name='task1')(shared_layers)
+      output_task2 = Dense(2, activation='softmax', name='task2')(shared_layers)
+      output_task3 = Dense(1, activation='linear', name='task3')(shared_layers)
+
+      model = Model(inputs=input_layer, outputs=[output_task1, output_task2, output_task3])
+
+      model.compile(optimizer='adam',
+                  loss={
+                        'task1': 'sparse_categorical_crossentropy',
+                        'task2': 'sparse_categorical_crossentropy',
+                        'task3': 'mean_squared_error'
+                  },
+                  metrics={
+                        'task1': 'accuracy',
+                        'task2': 'accuracy',
+                        'task3': 'mean_squared_error' 
+                  })
+      model.fit(X_train, [Y_train[:, 0], Y_train[:, 1], Y_train[:, 2]], epochs=50, batch_size=32, validation_split=0.2)
+      performance = model.evaluate(X_test, [Y_test[:, 0], Y_test[:, 1], Y_test[:, 2]], verbose=0)
+      embedding_model = Model(inputs=model.input, outputs=model.get_layer('finaldense').output)
+      embeddings = embedding_model.predict(X_combined)
+      return embeddings
 
 
-
-output_task1 = Dense(5, activation='softmax', name='output_task1')(shared_layers)
-output_task2 = Dense(9, activation='softmax', name='output_task2')(shared_layers)
-output_task3 = Dense(2, activation='softmax', name='output_task3')(shared_layers)
-
-model = Model(inputs=input_layer, outputs=[output_task1, output_task2, output_task3])
-
-model.compile(optimizer=tf.keras.optimizers.Adam(),
-              loss={'output_task1': 'sparse_categorical_crossentropy',
-                    'output_task2': 'sparse_categorical_crossentropy',
-                    'output_task3': 'sparse_categorical_crossentropy'},
-              metrics=['accuracy'])
-
-model.fit(X_train, [Y_train[:, 0], Y_train[:, 1], Y_train[:, 2]], epochs=10, batch_size=32, validation_split=0.2)
-performance = model.evaluate(X_test, [Y_test[:, 0], Y_test[:, 1], Y_test[:, 2]], verbose=0)
-
-
-embeddings = model.get_layer(index=-3).get_weights()[0]
+# for x in range(1,Porto_data['cluster'].unique()+1):
+#      all_embeddings_per_cluster = []
+#      all_embeddings_per_cluster.append(NeuralNet(Porto_data))
+ 
